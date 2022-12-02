7428bf46173436f2686aebd8bd2605307143947e
frans <frans.hoekstra@gmail.com>
2022-12-02 17:24:24 +0100

    less code = less bugs

--- python/day2.py
+++ python/day2.py
@@ -1,17 +1,14 @@
 def score(me, opponent):
-    round = game[me][0]
-    if me == conv[opponent]: round += 3
-    elif me == game[conv[opponent]][1]: round += 6
-    return round
+    if me == conv[opponent]: return 3 + game[me][0]
+    if me == game[conv[opponent]][1]: return 6 + game[me][0]
+    return game[me][0]
 
-input, game, conv, b = [line.strip() for line in open("../input/day2.txt", "r")], {"X": [1, "Y", "Z"], "Y": [2, "Z", "X"], "Z": [3, "X", "Y"]}, {"A": "X", "B": "Y", "C": "Z"}, 0
+input, game, conv, a, b = [line.strip() for line in open("../input/day2.txt", "r")], {"X": [1, "Y", "Z"], "Y": [2, "Z", "X"], "Z": [3, "X", "Y"]}, {"A": "X", "B": "Y", "C": "Z"}, 0, 0
 
-print(sum([score(l.split(" ")[1], l.split(" ")[0]) for l in input]))
-for l in input:
-    opponent, outcome = l.split(" ")[0], l.split(" ")[1]
-    match outcome:
-        case "Y": me = conv[opponent]
-        case "Z": me = game[conv[opponent]][1]
-        case "X": me = game[conv[opponent]][2]
-    b += score(me, opponent)
-print(b)
+for i, j in [l.split(" ") for l in input]:
+    a += score(j,i)
+    match j:
+        case "Y": b += score(conv[i], i)
+        case "Z": b += score(game[conv[i]][1], i)
+        case "X": b += score(game[conv[i]][2], i)
+print(a, b)
