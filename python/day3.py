0d2e5524063fd81287a0632e1f0e641632f52305
frans <frans.hoekstra@gmail.com>
2022-12-03 09:04:03 +0100

    less is more

--- python/day3.py
+++ python/day3.py
@@ -1,11 +1,8 @@
-f, a, b, first, second = [line.strip() for line in open("../input/day3.txt", "r")], 0, 0, "", ""
-for line, secondhalf in [(l, l[len(l) // 2 :]) for l in f]:
-    c = [c for c in line if c in secondhalf][0]
+f, a, b = [line.strip() for line in open("../input/day3.txt", "r")], 0, 0
+for index, item in enumerate([(l, l[len(l) // 2 :]) for l in f]):
+    c = [c for c in item[0] if c in item[1]][0]
     a += ord(c) - 96 if c.islower() else ord(c) - 38
-    if len(first) == 0: first = line
-    elif len(second) == 0: second = line
-    else:
-        c = [c for c in first if c in second and c in line][0]
+    if (index + 1) % 3 == 0:
+        c = [c for c in f[index-2] if c in f[index-1] and c in item[0]][0]
         b += ord(c) - 96 if c.islower() else ord(c) - 38
-        first, second = "", ""
 print(a, b)
