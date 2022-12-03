6cbb8a4d92e91e20e3f0cfd25b4df19ffb96918f
frans <frans.hoekstra@gmail.com>
2022-12-03 08:25:41 +0100

    day3

---
+++ python/day3.py
@@ -0,0 +1,15 @@
+f, a, b, first, second = [line.strip() for line in open("../input/day3.txt", "r")], 0, 0, "", ""
+for line in f:
+    for c in line[: len(line) // 2]:
+        if c in line[len(line) // 2 :]:
+            a += ord(c) - 96 if c.islower() else ord(c) - 38
+            break
+    if len(first) == 0: first = line
+    elif len(second) == 0: second = line
+    else:
+        for c in first:
+            if c in second and c in line:
+                b += ord(c) - 96 if c.islower() else ord(c) - 38
+                break
+        first, second = "", ""
+print(a, b)
