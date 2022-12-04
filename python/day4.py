4cdee3b0810cbb80482caeadc380f919639d5985
frans <frans.hoekstra@gmail.com>
2022-12-04 17:15:39 +0100

    day4

---
+++ python/day4.py
@@ -0,0 +1,5 @@
+f, a, b = [line.strip().split(",") for line in open("../input/day4.txt", "r")], 0, 0
+for i, j, k, l in [((int)(s[0].split("-")[0]), (int)(s[0].split("-")[1]), (int)(s[1].split("-")[0]), (int)(s[1].split("-")[1])) for s in f]:
+    a += 1 if (i >= k and j <= l) or (k >= i and l <= j) else 0
+    b += 1 if (i >= k and i <= l) or (j >= k and j <= l) or (k >= i and k <= j) or (l >= i and l <= j) else 0
+print(a, b)
