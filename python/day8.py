bc88bf45499db6431ffb1b206e4af276e2ea4f81
frans <frans.hoekstra@gmail.com>
2022-12-08 23:26:02 +0100

    day8 little bit better

--- python/day8.py
+++ python/day8.py
@@ -4,32 +4,18 @@ for y, line in enumerate(f):
         trees[(x, y)] = c
 for i in range(x + 1):
     for j in range(y + 1):
-        if i == 0 or j == 0 or i == x or j == y:
-            a += 1
+        if i == 0 or j == 0 or i == x or j == y: a += 1
         else:
-            visible = True
+            a1, a2, a3, a4 = True, True, True, True
             for k in range(i):
-                if trees[(k, j)] >= trees[(i, j)]: visible = False
-            if visible: a += 1
-            else:
-                visible = True
-                for k in range(j):
-                    if trees[(i, k)] >= trees[(i, j)]: visible = False
-                if visible: a += 1
-                else:
-                    visible = True
-                    for k in range(x - i):
-                        if trees[(i + k + 1, j)] >= trees[(i, j)]: visible = False
-                    if visible: a += 1
-                    else:
-                        visible = True
-                        for k in range(y - j):
-                            if trees[(i, j + k + 1)] >= trees[(i, j)]: visible = False
-                        if visible: a += 1
-print(a)
-for i in range(x + 1):
-    for j in range(y + 1):
-        if i > 0 and j > 0 and i < x and j < y:
+                if trees[(k, j)] >= trees[(i, j)]: a1 = False
+            for k in range(j):
+                if trees[(i, k)] >= trees[(i, j)]: a2 = False
+            for k in range(x - i):
+                if trees[(i + k + 1, j)] >= trees[(i, j)]: a3 = False
+            for k in range(y - j): 
+                if trees[(i, j + k + 1)] >= trees[(i, j)]: a4 = False
+            if a1 or a2 or a3 or a4: a+=1
             b1, b2, b3, b4 = 0, 0, 0, 0
             for k in range(i):
                 b1 += 1
@@ -43,6 +29,6 @@ for i in range(x + 1):
             for k in range(y - j):
                 b4 += 1
                 if trees[(i, j + k + 1)] >= trees[(i, j)]: break
-            if b1 * b2 * b3 * b4 > b:
-                b = b1 * b2 * b3 * b4
+            if b1 * b2 * b3 * b4 > b: b = b1 * b2 * b3 * b4
+print(a)
 print(b)
