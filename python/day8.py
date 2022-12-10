19ca838846ff1b78f19a0808b839c8cf2ade3dad
frans <frans.hoekstra@gmail.com>
2022-12-10 11:53:06 +0100

    day8 in 8 (12 - 4 spare of day10) lines

--- python/day8.py
+++ python/day8.py
@@ -1,26 +1,12 @@
 f, trees, a, b = [line.strip() for line in open("../input/day8.txt", "r")], {}, 0, 0
 for y, line in enumerate(f):
-    for x, c in enumerate(line):
-        trees[(x, y)] = c
-for i in range(x + 1):
-    for j in range(y + 1):
-        if i == 0 or j == 0 or i == x or j == y: a += 1
-        else:
-            a1, a2, a3, a4 = True, True, True, True
-            b1, b2, b3, b4 = 0, 0, 0, 0
-            for k in range(i):
-                if a1: b1 += 1 
-                if trees[(i - k - 1, j)] >= trees[(i, j)]: a1 = False
-            for k in range(j):
-                if a2: b2 += 1
-                if trees[(i, j - k - 1)] >= trees[(i, j)]: a2 = False
-            for k in range(x - i):
-                if a3: b3 += 1
-                if trees[(i + k + 1, j)] >= trees[(i, j)]: a3 = False
-            for k in range(y - j): 
-                if a4: b4 += 1
-                if trees[(i, j + k + 1)] >= trees[(i, j)]: a4 = False
-            if a1 or a2 or a3 or a4: a += 1
-            if b1 * b2 * b3 * b4 > b: b = b1 * b2 * b3 * b4
-print(a)
-print(b)
+    for x, c in enumerate(line): trees[(x, y)] = (int)(c)
+for i in range(1, x):
+    for j in range(1, y):
+        thisa, thisb, h = [True] * 4, [0] * 4, trees[(i, j)]
+        for num, size, xoff, yoff in [(0, i, -1, 0), (1, j, 0, -1), (2, x - i, 1, 0), (3, y - j, 0, 1)]:
+            for k in range(size):
+                thisb[num], thisa[num] = thisb[num] + 1 if thisa[num] else thisb[num], False if trees[(i + k * xoff + xoff, j + k * yoff + yoff)] >= h else thisa[num]
+        if True in thisa: a += 1
+        b = max([b, thisb[0] * thisb[1] * thisb[2] * thisb[3]])
+print(a + x * 2 + y * 2, b)
