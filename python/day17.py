7bb8ecd09c3d518651bd10c0c858b3c83903053a
frans <frans.hoekstra@gmail.com>
2022-12-17 11:35:22 +0100

    day17 part2

--- python/day17.py
+++ python/day17.py
@@ -1,22 +1,9 @@
-def printgird(grid):
-    ymax=0
-    for point in grid:
-        ymax = max(point[1], ymax)
-    for y in range(ymax, 0 - 1, -1):
-        line = "|"
-        for x in range(0, 7):
-            if (x, y) in grid: line += "#"
-            else: line += "."
-        line += "|"
-        print(line)
-
 rocks = [((0,0),(1,0),(2,0),(3,0)), ((1,0),(0,1),(1,1),(2,1),(1,2)), ((0,0),(1,0),(2,0),(2,1),(2,2)), ((0,0),(0,1),(0,2),(0,3)), ((0,0),(1,0),(0,1),(1,1))]
-#gas = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 gas = [line.strip() for line in open("../input/day17.txt", "r")][0]
 moves = {'<': (-1, 0), '>': (1, 6)}
-grid, maxy, nextrock, gaspos = set(), 0, 0, 0
+grid, maxy, nextrock, gaspos, codes, i = set(), 0, 0, 0, {}, 0
 
-for i in range(2022):
+while i < 1000000000000:
     newrock = set()
     for (rx, ry) in rocks[nextrock]: newrock.add((2 + rx, maxy + 3 + ry))
     nextrock = 0 if nextrock == 4 else nextrock + 1
@@ -46,6 +33,25 @@ for i in range(2022):
             move = False
     for point in newrock: grid.add(point)
     maxy = max([y for x,y in grid]) + 1
-
-printgird(grid)
-print(maxy)
+    newgrid = set()
+    for point in grid:
+        if point[1] > maxy - 50:
+            newgrid.add(point)
+    grid = newgrid
+    if i == 2022: print("a:", maxy)
+    i += 1
+    if i > 2000 and i < 100000:
+        code = ""
+        for j in range(40):
+            for x in range(7):
+                if (x, maxy - j) in grid: code += (str)(x)
+        if code not in codes: codes[code] = (i, maxy)
+        else:
+            dy, di = maxy - codes[code][1], i - codes[code][0] 
+           # print("in", di, "stappen gaat y", dy, "omhoog")
+            steps = (1000000000000 -i -di) // di
+            i, maxy = i + steps * di, maxy + steps * dy
+            newgrid = set()
+            for point in grid: newgrid.add((point[0],point[1] + steps * dy))
+            grid = newgrid
+print("b:", maxy)
