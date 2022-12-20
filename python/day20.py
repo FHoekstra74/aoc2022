f12917e78ef3d6fdbffde6c24b0d995682e1c219
frans <frans.hoekstra@gmail.com>
2022-12-20 19:35:54 +0100

    day20

---
+++ python/day20.py
@@ -0,0 +1,18 @@
+from collections import deque
+class nr: 
+    def __init__(self, val): self.val = val
+f = [(int)(line.strip()) for line in open("../input/day20.txt", "r")]
+for run in range(2):
+    numbers = deque(nr(number * (811589153 if run == 1 else 1)) for number in f)
+    premixed, res = list(numbers), 0
+    for _ in range(10 if run == 1 else 1):
+        for number in premixed:
+            numbers.rotate(-numbers.index(number))
+            numbers.popleft()
+            numbers.rotate(-number.val)
+            numbers.insert(0, number)
+    while numbers[0].val != 0: numbers.rotate(-1)
+    for _ in range(3):
+        for _ in range(1000): numbers.rotate(-1)
+        res += numbers[0].val
+    print(res)
