790fa130a91d5abea27d61cea5d715aa295050b6
frans <frans.hoekstra@gmail.com>
2022-12-21 19:09:43 +0100

    12 lines of day21

--- python/day21.py
+++ python/day21.py
@@ -1,31 +1,13 @@
-def calc (inst, mode):
+def calc (inst, partb, humn):
+    if partb: inst["humn"] = humn
     while True:
-        for k,v in inst.items():
-            if type(v) is str:
-                i = v.split()
-                if type(inst[i[0]]) is not str and type(inst[i[2]]) is not str:
-                    num1, num2, res  = inst[i[0]], inst[i[2]], 0
-                    if i[1] == "+": res = num1 + num2
-                    elif i[1] == "-": res = num1 - num2
-                    elif i[1] == "*": res = num1 * num2
-                    elif i[1] == "/": res = num1 / num2
-                    inst[k] = res
-                    if k == "root": return res if mode == 1 else num1 - num2
-
-f, inst = [line.strip() for line in open("../input/day21.txt", "r")], {}
-for line in f:
-    h = line.split(":")
-    inst[h[0]] = (int)(h[1].strip()) if h[1].strip().isnumeric() else h[1].strip()
-print("a:", calc(inst.copy(),1))
-mini, maxi = 0, 10000000000000
-while True:
-    mid = (mini + maxi) // 2
-    cp = inst.copy()
-    cp["humn"] = mid
-    res = calc(cp, 2)
-    if res > 0: mini = mid
-    elif res < 0: maxi = mid
-    else:
-        print("b:", mid)
-        break
+        for k, v in [(key,value.split()) for (key,value) in inst.items() if type(value) is str and type(inst[value.split()[0]]) is not str and type(inst[value.split()[2]]) is not str]:
+            inst[k] = inst[v[0]] + inst[v[2]] if v[1] == "+" else inst[v[0]] - inst[v[2]] if v[1] == "-" else inst[v[0]] * inst[v[2]] if v[1] == "*" else inst[v[0]] / inst[v[2]]
+            if k == "root": return inst[v[0]] - inst[v[2]] if partb else inst[k]
+inst, mini, maxi, res = {h[0]: (int)(h[1].strip()) if h[1].strip().isnumeric() else h[1].strip() for h in [l.strip().split(":") for l in open("../input/day21.txt", "r")]}, 0, 10000000000000, 1
+print("a:", (int)(calc(inst.copy(), False, 0)))
+while res != 0:
+    res = calc(inst.copy(), True, (mini + maxi) // 2)
+    if res == 0: print("b:", (mini + maxi) // 2)
+    mini, maxi = ((mini + maxi) // 2, maxi) if res > 0 else (mini, (mini + maxi) // 2)
     
